e776aa3884ef5f95bedb57ef089921ead3968fb7
Furkan Gezek <furkangezek2014@gmail.com>
2023-03-27 09:45:38 +0300

    chore(services/graphql): remove unused type

--- services/graphql/resolver/pano_post_mutation.go
+++ services/graphql/resolver/pano_post_mutation.go
@@ -17,10 +17,6 @@ type CreatePanoPostArgs struct {
 	Input *CreatePanoPostInput
 }
 
-type PanoMutationResolver struct {
-	Clients *clients.Clients
-}
-
 func NewCreatePanoPostMutationResolver(ctx context.Context, clients *clients.Clients, args *CreatePanoPostArgs) (*PanoPostResolver, error) {
 	createdPost, err := clients.PanoAPI.CreatePost(ctx, &pano_api.CreatePostRequest{
 		Title:   args.Input.Title,
